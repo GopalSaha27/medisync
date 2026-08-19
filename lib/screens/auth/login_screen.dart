e130f9868bcc202067855d4916e8af9e0480526a
GopalSaha27 <iamgopalsaha27@gmail.com>
2026-08-19 21:50:47 +0600

    create login screen

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'register_screen.dart';
+import '../../widgets/app_text_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+        ),
+      ),
+    );
+  }
+}
