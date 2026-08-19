36dc023368eac37001cee754cad3879521481986
GopalSaha27 <iamgopalsaha27@gmail.com>
2026-08-20 01:26:13 +0600

    Work on login screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -17,6 +17,48 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const SizedBox(height: 45),
+
+              //work MediSync icon...........
+              Center(
+                child: Container(
+                  height: 75,
+                  width: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.teal.shade50,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.medication_rounded,
+                    size: 40,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              // work welcome text........
+              const Center(
+                child: Text(
+                  'Welcome to MediSync',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              //work subtitle............
+              Center(
+                child: Text(
+                  'Manage your medication with ease'
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
