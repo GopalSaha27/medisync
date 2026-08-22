e3efb26efdfcc1853ed52d70216c919e9ded7e94
GopalSaha27 <iamgopalsaha27@gmail.com>
2026-08-22 13:07:11 +0600

    add fields and controller,works on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'screens/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
-    runApp(const MediSync());
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MediSync());
 }
 
 class MediSync extends StatelessWidget {
@@ -11,16 +17,16 @@ class MediSync extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "MediSync",
-        theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.teal,
-            ),
-            fontFamily: 'Roboto'
+      debugShowCheckedModeBanner: false,
+      title: 'MediSync',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
         ),
-        home: const LoginScreen(),
+        fontFamily: 'Roboto'
+      ),
+      home: const LoginScreen(),
     );
   }
 }
--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/material.dart';
 
 class AppTextField extends StatelessWidget {
+  final TextEditingController? controller;
   final String label;
   final String hint;
   final IconData prefixIcon;
-  final bool obscureText; //To hide text if it is a password.
-
-
+  final bool obscureText;
+  final VoidCallback? onSuffixIconPressed;
+  final IconData? suffixIcon;
   const AppTextField({
     super.key,
+    this.controller,
     required this.label,
     required this.hint,
     required this.prefixIcon,
+
     this.obscureText = false,
-  });
+    this.onSuffixIconPressed,
+    this.suffixIcon,
+    });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       obscureText: obscureText,
       decoration: InputDecoration(
         labelText: label,
         hintText: hint,
         prefixIcon: Icon(prefixIcon),
+
+        
+        suffixIcon: suffixIcon != null
+            ? IconButton(
+                onPressed: onSuffixIconPressed,
+                icon: Icon(suffixIcon),
+              )
+              : null,
+
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(14),
         ),
+
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(14),
           borderSide: const BorderSide(
             width: 2,
             color: Colors.teal,
-          )
-        )
+          ),
+        ),      
       ),
     );
   }
